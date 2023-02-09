805b54928839dbf3b6797e167420bc4b7fbbf30a
AbdimulhinYussuf3675 <adamabdimulhi.001@gmail.com>
2023-02-09 21:33:58 +0300

    UUpdated for the comment page

--- src/index.js
+++ src/index.js
@@ -1,19 +1,64 @@
-import './style.css';
-import movies from './modules/fetchapi.js';
+import addcomment from './modules/addcomment.js';
+import commentForm from './modules/comment.js';
+import fetchapi from './modules/fetchapi.js';
 import fetchlike from './modules/fetchlike.js';
-import moviecount from './modules/moviecount.js';
+import listlayout from './modules/listlayout.js';
 import newlike from './modules/newlike.js';
 import layout from './modules/pagelayout.js';
+import poptemp from './modules/poptemplate.js';
+import './style.css';
+
+const movies = async () => {
+  const res = await fetch('https://api.tvmaze.com/search/shows?q=girls');
+  const data = await res.json();
+  return data;
+};
 
 movies().then((movie) => {
   movie.map((each, index) => {
     layout(each);
     const num = document.querySelector('.total');
-    num.innerText = moviecount(movie);
+    num.innerText = movie.length;
     const heart = document.querySelectorAll('#heart');
     const card = document.querySelectorAll('.list');
     fetchlike(card);
     newlike(heart, index, each.show.id);
+    const comment = document.querySelectorAll('#comment');
+    comment.forEach((com, ind) => {
+      com.addEventListener('click', () => {
+        if (index === ind) {
+          const lists = [];
+          poptemp(each, 'comment');
+          commentForm();
+          // fetch api
+          fetchapi(each.show.id, lists, 'comment');
+
+          // add comment
+          const form = document.querySelector('form');
+          const user = document.querySelector('#name');
+          const text = document.querySelector('#text');
+          const comsec = document.querySelector('.test');
+          form.addEventListener('submit', (e) => {
+            e.preventDefault();
+            addcomment(each.show.id, user.value, text.value);
+            lists.push({ creation_date: 'few minutes ago', username: user.value, comment: text.value });
+            comsec.innerHTML = '';
+            lists.map((e) => { listlayout(e, comsec); });
+            user.value = '';
+            text.value = '';
+            const header = document.querySelector('.head');
+            header.innerHTML = `comment ${lists.length}`;
+          });
+          // to cancel the pop up
+          const X = document.querySelector('.X');
+          X.addEventListener('click', () => {
+            const pop = document.querySelector('.pop');
+            pop.remove();
+            pop.innerrHTML = '';
+          });
+        }
+      });
+    });
     return each;
   });
 });
---
+++ src/modules/poptemplate.js
@@ -0,0 +1,30 @@
+const poptemp = (data, section) => {
+  const main = document.querySelector('main');
+  const temp = `
+            <div class="pup">
+                <div class="X">
+                    <i class="fa-solid fa-x"></i>
+                </div>
+              <div id="img-summary" class = ident>
+                <div class="image popup-image">
+                    <img src=${data.show.image.medium} alt="">
+                </div class="popup-summary">
+                <div>
+                 <h3> ${data.show.name}</h3>
+                <p>${data.show.summary}</p>
+                </div>
+            </div>
+            <div class="comment">
+                <h4 class="head">${section}()</h4>
+                <ul class="test list-test-reserve">
+                </ul>
+            </div>
+            </div>
+        `;
+  const pop = document.createElement('div');
+  pop.classList.add('pop');
+  pop.setAttribute('data-id', data.show.id);
+  pop.innerHTML = temp;
+  main.appendChild(pop);
+};
+export default poptemp;
