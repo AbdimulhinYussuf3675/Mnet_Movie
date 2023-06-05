69e2572281d438e251afc4208559a178c8f36055
Donmark2k <chukwuemekavp@gmail.com>
2023-06-05 10:39:48 +0100

    Add the reserve form

--- src/modules/moviecount.js
+++ src/modules/moviecount.js
@@ -1,5 +1,6 @@
 const moviecount = (movie) => {
   const size = movie.length;
+  return size;
 };
 
 export default moviecount;
--- src/modules/reserve.js
+++ src/modules/reserve.js
@@ -3,8 +3,8 @@ const reserveForm = () => {
   pup.innerHTML += ` <div class="addReserve">
                 <h4 class="reserve-h4">Add Reservation</h4>
                 <form class="res-form">
-                <input type="text" placeholder="your name" id="name" required>
-                <input id="start-date" type="date" placeholder="start date" required />
+                <input type="text" placeholder="your name" id="name" >
+                <input id="start-date" type="date" placeholder="start date"  />
                 <input id="end-date" type="date" placeholder="end date" required />
 
                 <button class="reserve-btn">submit</button>
